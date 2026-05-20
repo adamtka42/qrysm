f0d910d3312964c95c0c0f6eed2798df0eb94aa2
Adam Tkaczyk <adamtkaczyk@singularitysystems.net>
2026-05-20 23:30:03 +0200

    Fix qrysm strict deps and fixtures

--- beacon-chain/rpc/qrysm/v1alpha1/beacon/assignments_test.go
+++ beacon-chain/rpc/qrysm/v1alpha1/beacon/assignments_test.go
@@ -276,7 +276,7 @@ func TestServer_ListAssignments_HandlesUnscheduledValidator(t *testing.T) {
 	ctx := context.Background()
 	count := 64
 	validators := make([]*qrysmpb.Validator, 0, count)
-	withdrawCreds := make([]byte, 32)
+	withdrawCreds := make([]byte, field_params.WithdrawalCredentialsLength)
 	for i := range count {
 		pubKey := make([]byte, field_params.MLDSA87PubkeyLength)
 		binary.LittleEndian.PutUint64(pubKey, uint64(i))
